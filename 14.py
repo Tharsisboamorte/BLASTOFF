a016bd0f78945ac0d5769114e8d05363bd055bd5
Tharsis Lamin <85424269+Tharsisboamorte@users.noreply.github.com>
2021-10-21 04:20:36 -0300

    Delete 14.py

--- 14.py
+++
@@ -1,5 +0,0 @@
-palavra = input("Insira uma palavra para descobrir se ela é um palindormo")
-if str(palavra) == "".join(reversed(palavra)) :
-    print("Palíndromo")
-else:
-    print("Não é um palíndromo")
